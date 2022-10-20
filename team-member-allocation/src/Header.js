205f1db2834e0a087f5a8bb2fd2f46d20543b0c2
bruna_silva <bruna.silva@brcaptura.com.br>
2022-10-20 08:38:45 -0300

    first commit

---
+++ team-member-allocation/src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return(
+        <header>
+            <h1 className="bg-primary text-white rounded">Header</h1>
+        </header>
+    )
+}
+
+export default Header;
